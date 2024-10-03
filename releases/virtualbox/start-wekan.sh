4e0d1e2a1b1ed6faf6793c53f386b48ac777fb0a
HQscolt <tiogalatas@outlook.com>
2024-10-03 13:09:24 -0300

    Descrição das alterações feitas

--- releases/virtualbox/start-wekan.sh
+++ releases/virtualbox/start-wekan.sh
@@ -15,8 +15,8 @@
       #---------------------------------------------
       # https://github.com/wekan/wekan/wiki/Troubleshooting-Mail
       # https://github.com/wekan/wekan-mongodb/blob/master/docker-compose.yml
-      export MAIL_URL='smtp://user:pass@mailserver.example.com:25/'
-      export MAIL_FROM='Wekan Boards <info@example.com>'
+      export MAIL_URL='smtp://noc4.mgptelecom@gmail.com:kpghlggcghhmotro@smtp.gmail.com:587/'
+      export MAIL_FROM='Wekan Boards <noc4.mgptelecom@gmail.com>'
       #export MAIL_SERVICE=Outlook365
       #export MAIL_SERVICE_USER=firstname.lastname@hotmail.com
       #export MAIL_SERVICE_PASSWORD=SecretPassword
